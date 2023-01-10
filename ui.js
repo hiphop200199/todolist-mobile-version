42f4130c364d0688a92f75478857c6ee001d4955
Eric Tsai <hiphop200199@gmail.com>
2023-01-10 23:53:41 +0800

    介面調整

--- ui.js
+++ ui.js
@@ -2,18 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
    
     let musicSrc=new Audio("./melody.mp3");
     let musicOn=document.querySelector(".background-music");
-    let edit = document.querySelector(".edit");
-    let cover=document.querySelector(".cover");
     let paper = document.querySelector(".paper");
-    let modal=document.querySelector(".message");
-    let message=document.querySelector(".modal-message")
-    let closeBtn=document.querySelector(".closebtn");
-    let finish=document.querySelector(".finish");
-    bye.addEventListener("click",goodbye);
-    musicOn.addEventListener("click",musicMode);
-    edit.addEventListener("click",openNote);
-    closeBtn.addEventListener("click",closedialog);
-    finish.addEventListener("click",closeNote);
+  
+    musicOn.addEventListener("touchstart",musicMode);
+    
+    closeBtn.addEventListener("touchstart",closedialog);
+  
    
    
     function musicMode(){
